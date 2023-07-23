5ae9d78ec1c454d5d2fcc23f5742dcce1b9a7b93
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-07-23 18:26:35 +0530

    Mario script.js is an under construction

--- Script.js
+++ Script.js
@@ -1,11 +1,49 @@
-//Start Game button for DOM manipulation:-
-document.getElementById("start-btn").addEventListener("click",startGame);
+//First of all lets declare the variables:-
+let mario = document.getElementById("mario");
+let pipe = document.getElementById("pipe");
+let mushroom = document.getElementById("mushroom");
+let score = document.getElementById("score");
+var backgroundMusic = document.getElementById("background-music");
+let jumpSound = document.getElementById("jump-sound");
+let startBtn = document.getElementById("start-btn");
 
-//Function startbutton
-function startGame(){
-     // When i click on the start button will go out and the game page will be open.
-    document.getElementById("start-btn").style.display = "none";
-    // As soon as i click on the start button the entire whole game page will be open.
+
+//Second start button whenever the user get click to the button
+startBtn.addEventListener("click", function () {
+    
+    backgroundMusic.play();
+    // Hide the start button
+    startBtn.style.display = "none";
     document.getElementById("game-container").style.display = "block";
+    startGame();
+  });
 
-}
+
+  // Start the game here
+
+  function startGame()
+  {
+
+// Intialized the game variables:-
+  let marioJumping = false;
+  let marioMovingRight = false;
+  let marioMovingLeft = false;
+  let obstacles = [pipe, mushroom];
+  let gameScore = 0;
+  let gameContainerWidth = document.getElementById("game-container").offsetWidth;
+  let marioPosition = 50;
+
+    
+
+
+
+  }
+
+
+
+
+
+
+
+
+
